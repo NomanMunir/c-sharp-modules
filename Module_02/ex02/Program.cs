e68abe37bbd8bc6f78eaa89f0ada6fd98d7f175e
Nauman Munir <mrnomanmunir@gmail.com>
2026-02-17 14:54:46 +0400

    feat: implement Vector2 struct with operator overloading and equality checks

--- Module_02/ex02/Program.cs
+++ Module_02/ex02/Program.cs
@@ -5,13 +5,57 @@
 
 // TODO: Create struct Vector2
 
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+public struct Vector2(float x, float y)
+{
+    public float X { get; } = x;
+    public float Y { get; } = y;
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is Vector2 v && this == v;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public static Vector2 operator +(Vector2 a, Vector2 b)
+    {
+        return new Vector2(a.X + b.X, a.Y + b.Y);
+    }
+
+    public static Vector2 operator -(Vector2 a, Vector2 b)
+    {
+        return new Vector2(a.X - b.X, a.Y - b.Y);
+    }
+    public static Vector2 operator *(Vector2 a, float scalar)
+    {
+        return new Vector2(a.X * scalar, a.Y * scalar);
+    }
+    public static bool operator ==(Vector2 a, Vector2 b)
+    {
+        return a.X == b.X && a.Y == b.Y;
+    }
+    public static bool operator !=(Vector2 a, Vector2 b)
+    {
+        return !(a == b);
+    }
+
+}
 class Program
 {
     static void Main(string[] args)
     {
-        /* Uncomment when ready
-        Vector2 a = new Vector2(1.0f, 2.0f);
-        Vector2 b = new Vector2(3.0f, 4.0f);
+        Vector2 a = new(1.0f, 2.0f);
+        Vector2 b = new(3.0f, 4.0f);
 
         Console.WriteLine(a + b);       // (4, 6)
         Console.WriteLine(a - b);       // (-2, -2)
@@ -19,8 +63,7 @@ class Program
         Console.WriteLine(a == b);      // False
         Console.WriteLine(a != b);      // True
 
-        Vector2 c = new Vector2(1.0f, 2.0f);
+        Vector2 c = new(1.0f, 2.0f);
         Console.WriteLine(a == c);      // True
-        */
     }
 }
